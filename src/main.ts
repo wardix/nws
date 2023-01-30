cfd0ff99cb880b615b5bb9ca1e91f29ef036c670
Wardi <wardi@nusa.net.id>
2023-01-30 08:04:12 +0700

    revert commit 5351f90d15bfbbc37db37e97f6ec5315e4965323

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const configService = app.get(ConfigService);
   const port = +configService.get('PORT');
-  const cors = JSON.parse(configService.get('CORS'));
-  app.enableCors({origin: cors})
   await app.listen(port);
 }
 bootstrap();
